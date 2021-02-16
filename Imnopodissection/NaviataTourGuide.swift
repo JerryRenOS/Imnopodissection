1b369cfd6ce7527df34e77364bd5abe2fc0ade5c
Jerry Ren <jerryren.ios@gmail.com>
2021-02-15 23:59:07 -0500

    NaviataTourGuide .startup

---
+++ Imnopodissection/NaviataTourGuide.swift
@@ -0,0 +1,19 @@
+//
+//  NaviataTourGuide.swift
+//  Imnopodissection
+//
+//  Created by Jerry Ren on 2/11/21.
+//
+
+
+import UIKit
+ 
+private func getViciousness(screen: String, module: String, bundle: String? = nil) -> UIBroker? {
+    guard let screenPath = readableFromPropertyListo(screen: screen, module: module, bundle: bundle) else {
+        return nil
+    }
+    return nil
+}
+private func readableFromPropertyListo(screen: String, module: String, bundle: String?) -> String? {
+    return String.init()
+}
