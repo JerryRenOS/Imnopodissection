4c33d4d071485acb3a602cace5d0f4dc32cefba6
Jerry Ren <jerryren.ios@gmail.com>
2021-02-20 23:58:39 -0500

    Revert "Revert "Bare minimum of invocativepolicyproperties""
    This reverts commit ff268e94c5339ca2aca88993c1bf8490e0a72026.

---
+++ Imnopodissection/InvocativePolicyProperties.swift
@@ -0,0 +1,32 @@
+//
+//  InvocativePolicyProperties.swift
+//  Imnopodissection
+//
+//  Created by Jerry Ren on 2/19/21.
+//
+
+import Foundation
+
+public class InvocativePolicyProperties {
+    var policyIdentifier: TransmissionPolicy
+    var paramas: [AnyHashable: Any]?
+    var responseHandler: TransmissionResponseHandler?
+    
+    public init(_ policyID: TransmissionPolicy) {
+        self.policyIdentifier = policyID
+    }
+    
+    public func withParamas(_ params: [AnyHashable: Any]) -> InvocativePolicyProperties {
+        self.paramas = params
+        return self
+    }
+}
+
+public enum TransmissionPolicy: String {
+    case user_journey_66 = "user_journey_66"
+    // etc.
+}
+
+public protocol TransmissionResponseHandler {
+    
+}
