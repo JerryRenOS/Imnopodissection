9394b3613adf02b922aac8fa7d75d6de86a7b2ca
Jerry Ren <jerryren.ios@gmail.com>
2021-03-02 23:34:35 -0500

    embedded dict-struct within transmissionconstants' struct

---
+++ Imnopodissection/TransmissionConstants.swift
@@ -0,0 +1,17 @@
+//
+//  TransmissionConstants.swift
+//  Imnopodissection
+//
+//  Created by Jerry Ren on 3/2/21.
+//
+
+import Foundation
+
+//TransmissionConstants.DictionaryKeys.blackBoxData
+
+public struct TransmissionConstants {
+    public struct DictionaryKeys {
+        public static let blackBoxData = "blackBoxData"
+    }
+}
+  
