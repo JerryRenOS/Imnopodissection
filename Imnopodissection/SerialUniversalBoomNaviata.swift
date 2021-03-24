43cb089c13a5a7f1c8b763176fa0aa1ae9df9106
Jerry Ren <jerryren.ios@gmail.com>
2021-03-23 23:16:09 -0400

    didset mainNaviatalController inout&

--- Imnopodissection/SerialUniversalBoomNaviata.swift
+++ Imnopodissection/SerialUniversalBoomNaviata.swift
@@ -5,12 +5,26 @@
 //  Created by Jerry Ren on 2/5/21.
 //
 
-import Foundation
+import UIKit
 
 public class SerialUniversalBoomNaviata {
     
     public var delegate: MapperProtocolNaviataModule?
     public static let sharedInstance = SerialUniversalBoomNaviata.init()
+    
+    var mainNaviatalController: UINavigationController? {
+        didSet {
+            if var mainNaviata = mainNaviatalController {
+                self.settingUpNaviatalController(&mainNaviata)
+            }
+        }
+    }
+          
+    private func settingUpNaviatalController(_ naviContr: inout UINavigationController) {
+        naviContr.setNavigationBarHidden(true, animated: false)
+        naviContr.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        naviContr.navigationBar.shadowImage = UIImage()
+    }
     
     public enum RootedView {
         case bmci
