c0ca1a91a27b51263b252c5bbeb280ca4077afbe
Jerry Ren <jerryren.ios@gmail.com>
2021-03-25 23:43:11 -0400

    Revert "didset mainNaviatalController inout&"
    This reverts commit 43cb089c13a5a7f1c8b763176fa0aa1ae9df9106.

--- Imnopodissection/SerialUniversalBoomNaviata.swift
+++ Imnopodissection/SerialUniversalBoomNaviata.swift
@@ -5,26 +5,12 @@
 //  Created by Jerry Ren on 2/5/21.
 //
 
-import UIKit
+import Foundation
 
 public class SerialUniversalBoomNaviata {
     
     public var delegate: MapperProtocolNaviataModule?
     public static let sharedInstance = SerialUniversalBoomNaviata.init()
-    
-    var mainNaviatalController: UINavigationController? {
-        didSet {
-            if var mainNaviata = mainNaviatalController {
-                self.settingUpNaviatalController(&mainNaviata)
-            }
-        }
-    }
-          
-    private func settingUpNaviatalController(_ naviContr: inout UINavigationController) {
-        naviContr.setNavigationBarHidden(true, animated: false)
-        naviContr.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-        naviContr.navigationBar.shadowImage = UIImage()
-    }
     
     public enum RootedView {
         case bmci
