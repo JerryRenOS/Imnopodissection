9a6d037751ffdb661076aad2bc9923d1acad6487
jer .explosion <jerryren.ios@gmail.com>
2021-08-02 23:17:48 -0400

    Creating open class ApolloURLSessionClient

---
+++ Imnopodissection/ApolloPodoro.swift
@@ -0,0 +1,12 @@
+//
+//  ApolloPodoro.swift
+//  Imnopodissection
+//
+//  Created by Jerry Ren on 8/2/21.
+//
+
+import Foundation
+
+open class ApolloURLSessionClient: NSObject, URLSessionDelegate, URLSessionTaskDelegate, URLSessionDataDelegate {
+    
+}
